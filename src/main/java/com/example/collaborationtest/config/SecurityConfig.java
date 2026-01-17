3691926aaeeb38ac7b9192f5778d96b642acd84c
TamasagIrina <147584133+TamasagIrina@users.noreply.github.com>
2026-01-17 20:15:04 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/collaborationtest/config/SecurityConfig.java
+++ src/main/java/com/example/collaborationtest/config/SecurityConfig.java
@@ -98,7 +98,7 @@ public class SecurityConfig {
         c.setAllowedOrigins(List.of("http://localhost:4200"));
         c.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         c.setAllowedHeaders(List.of("*")); 
-        c.setExposedHeaders(List.of("Authorization"));
+        // c.setExposedHeaders(List.of("Authorization"));
        // c.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource s = new UrlBasedCorsConfigurationSource();
         s.registerCorsConfiguration("/**", c);
