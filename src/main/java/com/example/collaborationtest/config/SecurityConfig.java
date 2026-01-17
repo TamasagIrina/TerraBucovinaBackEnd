2f1bd7dcaeeb0fc5617b080dc74bed46e5da16a6
TamasagIrina <147584133+TamasagIrina@users.noreply.github.com>
2026-01-18 00:57:03 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/collaborationtest/config/SecurityConfig.java
+++ src/main/java/com/example/collaborationtest/config/SecurityConfig.java
@@ -95,8 +95,7 @@ public class SecurityConfig {
     @Bean
    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration c = new CorsConfiguration();
-        c.setAllowedOrigins(List.of("http://localhost:4200",
-                                   "https://terrabucovinabackend-production.up.railway.app"));
+        c.setAllowedOrigins(List.of("*"));
         c.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         c.setAllowedHeaders(List.of("*")); 
         // c.setExposedHeaders(List.of("Authorization"));
