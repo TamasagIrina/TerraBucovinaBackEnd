d7f6e0ddf8f4020f468940b51ed8d273abf55263
teodo <teodor.binisor@gmail.com>
2026-02-15 18:05:08 +0200

    Fixed mail Service for railway

--- src/main/java/com/example/collaborationtest/service/EmailService.java
+++ src/main/java/com/example/collaborationtest/service/EmailService.java
@@ -18,11 +18,13 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import org.thymeleaf.context.Context;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import com.resend.*;
 
@@ -41,43 +43,77 @@ public class EmailService {
 
     @Autowired
     private TemplateEngine templateEngine;
-    @Autowired
-    private JavaMailSender mailSender;
 
     @Value("${brevo.sender.email:teodor.binisor@gmail.com}")
     private String senderEmail;
 
+    @Value("${brevo.api.key}")
+    private String apiKey;
+
+    @Value("${brevo.api.url}")
+    private String apiUrl;
+
+
+    private final WebClient webClient;
+
     public EmailService(
-                        ProductService productService, ImageService imageService, UserRepo userRepo) {
+            ProductService productService, ImageService imageService, UserRepo userRepo, WebClient.Builder webClientBuilder) {
         this.productService = productService;
         this.imageService = imageService;
         this.userRepo = userRepo;
+        this.webClient = webClientBuilder.build();
     }
 
 
 
  
+//    @Async
+//    public void sendEmail(String to, String subject, String body, String provider) {
+//
+//        try {
+//            MimeMessage message = mailSender.createMimeMessage();
+//            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+//
+//            helper.setFrom(senderEmail);
+//            helper.setTo(to);
+//            helper.setSubject(subject);
+//
+//            helper.setText(body, true);
+//            mailSender.send(message);
+//            System.out.println("Email sent successfully to " + to);
+//        } catch (Exception e) {
+//            System.err.println("CRITICAL ERROR IN ASYNC EMAIL: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//
+//    }
+
     @Async
     public void sendEmail(String to, String subject, String body, String provider) {
+        BrevoPayload payload = BrevoPayload.builder()
+                .sender(BrevoPayload.Sender.builder()
+                        .name("TerraBucovina")
+                        .email("irinatamasag2004@gmail.com") // Must be a verified sender in Brevo
+                        .build())
+                .to(Collections.singletonList(
+                        BrevoPayload.Recipient.builder()
+                                .email(to)
+                                .name("Valued User")
+                                .build()
+                ))
+                .subject(subject)
+                .htmlContent(body)
+                .build();
 
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-            helper.setFrom(senderEmail);
-            helper.setTo(to);
-            helper.setSubject(subject);
-
-            helper.setText(body, true);
-            mailSender.send(message);
-            System.out.println("Email sent successfully to " + to);
-        } catch (Exception e) {
-            System.err.println("CRITICAL ERROR IN ASYNC EMAIL: " + e.getMessage());
-            e.printStackTrace();
-        }
-
+        this.webClient.post()
+                .uri(apiUrl)
+                .header("api-key", apiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(payload)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
-
 
 
     @Async
